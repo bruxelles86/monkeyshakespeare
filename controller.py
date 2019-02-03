5e463a522e53ec3e25d9e2910b233f72c1634f9b
Shumai <shumai9@github.com>
2019-02-03 16:11:16 +0000

    addedd letter filter

--- controller.py
+++ controller.py
@@ -7,17 +7,16 @@ longestMatchLength = 0
 longestMatch = ""
 
 #need to open and read the file
-
+required_letters = list(string.ascii_letters)
 def book_reader():
   directory = "./shakespeare"
   for filename in os.listdir(directory):
     file = open(os.path.join(directory, filename), "r")
     for line in file:
       characters = list(line)
-      print(characters)
-      
+      print([char for char in characters if char in required_letters])
+
   #   open("./shakespeare/sample1", "r")
-  
 
 #generates letter
 # def rand_keystroke_generator(num):
