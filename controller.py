9cbab79865d60169dc7a5c936a4df23917bb1eba
Will Gant <w_gant@yahoo.co.uk>
2019-02-03 16:43:12 +0100

    WIP Iterating over sample files

--- controller.py
+++ controller.py
@@ -1,5 +1,6 @@
 import string
-import random 
+import random
+import os
 
 
 longestMatchLength = 0
@@ -7,22 +8,25 @@ longestMatch = ""
 
 #need to open and read the file
 
-
-def book_reder():
-  pass
-  file = open("./shakespeare/sample1", "r")
-  for line in file:
-    words = line.split(" ")
-    for word in words:
-      print(word)
+def book_reader():
+  directory = "./shakespeare"
+  for filename in os.listdir(directory):
+    file = open(os.path.join(directory, filename), "r")
+    for line in file:
+      characters = list(line)
+      print(characters)
+      
+  #   open("./shakespeare/sample1", "r")
+  
 
 #generates letter
-def rand_keystroke_generator(num):
-  a = 0
-  while a < num:
-    return random.choice(string.ascii_letters)
-    a += 1
+# def rand_keystroke_generator(num):
+#   a = 0
+#   while a < num:
+#     return random.choice(string.ascii_letters)
+#     a += 1
     
     
-print( rand_keystroke_generator(10))
+# print( rand_keystroke_generator(10))
+book_reader()
 
