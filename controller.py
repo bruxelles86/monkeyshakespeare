c042fff2a50d55b0a7745b203d5b4bf50b87d969
Will Gant <w_gant@yahoo.co.uk>
2019-02-11 21:03:41 +0100

    WIP growing our loop

--- controller.py
+++ controller.py
@@ -1,31 +1,39 @@
 import string
 import random
 import os
+import json
 
 counter = 0
 longestMatchLength = 0
 longestMatch = ""
 
-#need to open and read the file
 required_letters = list(string.ascii_letters)
 
-def book_reader(keystroke):
+with open('./shakespeare/samples.json') as f:
+  lines_to_check = json.load(f)
+
+
+def check(keystroke, count, lines_to_check):
   directory = "./shakespeare"
   for filename in os.listdir(directory):
     file = open(os.path.join(directory, filename), "r")
     for line in file:
       characters = list(line)
       chars_letters_only = [char for char in characters if char in required_letters]
-      if (len(chars_letters_only) < counter):
+      if (len(chars_letters_only) < count):
         continue
-      if (chars_letters_only[counter-1] == keystroke):
-        print(chars_letters_only[counter-1], "found on count", counter)
+      if (chars_letters_only[count-1] == keystroke):
+        print(chars_letters_only[count-1], "found on count", count)
 
 #generates letter
 def generate_keystroke():
   return random.choice(string.ascii_lowercase)
     
 while True:
-  counter += 1
-  key = generate_keystroke()
-  book_reader(key)
+  while all(len(work["lines"]) > 0 for work in lines_to_check["works"]):
+    counter += 1
+    key = generate_keystroke()
+    print(counter)
+    # check(key, counter, lines)
+  # if lines_to_check is empty, reload lines_to_check from file
+  # check if match > longestMatch
