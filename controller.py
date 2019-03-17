87e73c854b3a7ae3b6ecc9f5ad62d5a45dd531d4
Will Gant <w_gant@yahoo.co.uk>
2019-03-17 11:16:15 +0100

    WIP mess

--- controller.py
+++ controller.py
@@ -11,29 +11,35 @@ required_letters = list(string.ascii_letters)
 
 with open('./shakespeare/samples.json') as f:
   lines_to_check = json.load(f)
+  print(lines_to_check)
 
 
 def check(keystroke, count, lines_to_check):
-  directory = "./shakespeare"
-  for filename in os.listdir(directory):
-    file = open(os.path.join(directory, filename), "r")
-    for line in file:
-      characters = list(line)
-      chars_letters_only = [char for char in characters if char in required_letters]
-      if (len(chars_letters_only) < count):
-        continue
-      if (chars_letters_only[count-1] == keystroke):
-        print(chars_letters_only[count-1], "found on count", count)
+  for work in lines_to_check["works"]:
+    for line in work["lines"]:
+      
+
+  # directory = "./shakespeare"
+  # for filename in os.listdir(directory):
+  #   file = open(os.path.join(directory, filename), "r")
+  #   for line in file:
+  #     characters = list(line)
+  #     chars_letters_only = [char for char in characters if char in required_letters]
+  #     if (len(chars_letters_only) < count):
+  #       continue
+  #     if (chars_letters_only[count-1] == keystroke):
+  #       print(chars_letters_only[count-1], "found on count", count)
 
 #generates letter
 def generate_keystroke():
   return random.choice(string.ascii_lowercase)
     
 while True:
-  while all(len(work["lines"]) > 0 for work in lines_to_check["works"]):
+  while any(len(work["lines"]) > 0 for work in lines_to_check["works"]):
     counter += 1
     key = generate_keystroke()
-    print(counter)
-    # check(key, counter, lines)
+    check(key, counter, lines_to_check)
+
   # if lines_to_check is empty, reload lines_to_check from file
   # check if match > longestMatch
+
