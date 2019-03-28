c1c00138c3e9a1c40321127cd92e81c24a893070
Shumai <shumai9@github.com>
2019-03-28 14:52:52 +0000

    Changes to be committed: 	modified:   README.md -- added the steps the algo works 	modified:   controller.py --refactored check func and removed variables and changed check_line func logic 		  	 to free it from depending on counter, 			--moved the counter++ outside the 2nd loop 			 created a new func to spit random words rather than single letters so we can check for words to match

--- controller.py
+++ controller.py
@@ -8,43 +8,53 @@ longestMatchLength = 0
 longestMatch = ""
 
 required_letters = list(string.ascii_letters)
-
-with open('./shakespeare/samples.json') as f:
-  lines_to_check = json.load(f)
+#  created a method to load json object from sample
+def load_book():
+  with open('./shakespeare/samples.json') as f:
+    lines_to_check = json.load(f)
+    return lines_to_check
 
 def check(keystroke, counter, lines_to_check):
   def check_line(line):
-    if line[counter - 1].lower() == keystroke:
+    if any(leter.lower() == keystroke for leter in line):        
       return True
     else:
       return False
 
-  new_lines_to_check = {"works": []}
+  #refactored code below to reduce lengthy varaibles
+  new_lines_to_check = { "works": []}
 
-  for work in lines_to_check["works"]:
-    matched_lines = list(filter(check_line, work["lines"]))
-    new_work = { 
+  for work in lines_to_check["works"]: 
+    new_lines_to_check["works"].append({
       "name": work["name"],
-      "lines": matched_lines
-    }
-    new_lines_to_check["works"].append(new_work)
-  return new_lines_to_check       
-      
+      "lines": list(filter(check_line, work["lines"]))
+    })
+       
+  return new_lines_to_check
 
+# generates letter --we need to work on this whether to generate
+# a single leter or a combination of letters that a monkey can type i.e 'xlm'
+# maybe generate random word length as well 
+def monkey_typed_word():
+  return f"{generate_keystroke()}{generate_keystroke()}{generate_keystroke()}" 
 
-
-#generates letter
 def generate_keystroke():
   return random.choice(string.ascii_lowercase)
     
 while True:
+  word = monkey_typed_word()
+  print("the word is", word)
+  lines_to_check = load_book()
+  counter+=1
+  print("started again", counter)
+
   while any(len(work["lines"]) > 0 for work in lines_to_check["works"]):
-    counter+=1
     key = generate_keystroke()
-    matched_lines = check(key, counter, lines_to_check)
+    matched_lines = check(key, counter, lines_to_check)      
     lines_to_check = matched_lines
     print(lines_to_check, counter, key)
 
+  
   # if lines_to_check is empty, reload lines_to_check from file
   # check if match > longestMatch
 
