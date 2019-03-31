10b396717a858c9f73ee3d4c1cb25704a0c4db2f
Shumai <shumai9@github.com>
2019-03-31 20:31:41 +0100

    fixed logic of check function and printing longest match

--- controller.py
+++ controller.py
@@ -3,58 +3,57 @@ import random
 import os
 import json
 
+
 counter = 0
-longestMatchLength = 0
 longestMatch = ""
 
 required_letters = list(string.ascii_letters)
+
 #  created a method to load json object from sample
+
 def load_book():
   with open('./shakespeare/samples.json') as f:
-    lines_to_check = json.load(f)
-    return lines_to_check
+    return json.load(f)
+
 
 def check(keystroke, counter, lines_to_check):
   def check_line(line):
-    if any(leter.lower() == keystroke for leter in line):        
-      return True
-    else:
-      return False
+    if counter <= len(line):
+      if line[counter - 1].lower() == keystroke:   
+        return True
+      else: 
+        return False
+    return False
+
 
   #refactored code below to reduce lengthy varaibles
-  new_lines_to_check = { "works": []}
+  new_lines_to_check = {"works": []}
 
   for work in lines_to_check["works"]: 
     new_lines_to_check["works"].append({
       "name": work["name"],
       "lines": list(filter(check_line, work["lines"]))
-    })
-       
+    })    
   return new_lines_to_check
-
-# generates letter --we need to work on this whether to generate
-# a single leter or a combination of letters that a monkey can type i.e 'xlm'
-# maybe generate random word length as well 
-def monkey_typed_word():
-  return f"{generate_keystroke()}{generate_keystroke()}{generate_keystroke()}" 
 
 def generate_keystroke():
   return random.choice(string.ascii_lowercase)
     
 while True:
-  word = monkey_typed_word()
-  print("the word is", word)
   lines_to_check = load_book()
-  counter+=1
-  print("started again", counter)
-
+  counter = 0
   while any(len(work["lines"]) > 0 for work in lines_to_check["works"]):
     key = generate_keystroke()
+    counter+=1
     matched_lines = check(key, counter, lines_to_check)      
     lines_to_check = matched_lines
-    print(lines_to_check, counter, key)
+    if len(longestMatch) < counter: 
+      for work in lines_to_check["works"]: 
+        for line in work["lines"]:
+          longestMatch = line[0:counter]
+
+    print(longestMatch, counter, key)
 
   
   # if lines_to_check is empty, reload lines_to_check from file
   # check if match > longestMatch
-
